19e8867685999592500d285f192c4614054d2125
JRSumner <JoeRaySumner@gmail.com>
2023-07-15 10:06:22 +0100

    Remove Commented Out Code

--- PasswordPal.UI/Program.cs
+++ PasswordPal.UI/Program.cs
@@ -11,9 +11,6 @@ internal static class Program
 		// To customize application configuration such as set high DPI settings or default font,
 		// see https://aka.ms/applicationconfiguration.
 		ApplicationConfiguration.Initialize();
-		//Application.Run(new CreatePasswordForm());
-		//Application.Run(new StoredPasswordsForm());
 		Application.Run(new LoginForm());
-		//Application.Run(new RegistrationForm());
 	}
 }
