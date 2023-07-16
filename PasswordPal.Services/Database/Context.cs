8cb297d5cc64c5a1c52875b321fc9dfb3fa4aebd
JRSumner <JoeRaySumner@gmail.com>
2023-07-16 12:42:17 +0100

    Update Context, Delete SeedDatabase, Add Migration & Update Projects
    Update Context.cs to remove reference to deleted SeedDatabase.cs and utilize modelBuilder() directly within the Context class.
    
    Add InitialSeed migration, however issue is present with the connection string when running 'dotnet ef database update'
    
    Update project files to standardize references to the sqlServer package and add 'CopyToOutputDirectory' node to the project file for 'appsettings.json'

--- PasswordPal.Services/Database/Context.cs
+++ PasswordPal.Services/Database/Context.cs
@@ -1,35 +1,29 @@
 using PasswordPal.Core.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using System.Configuration;
 
 namespace PasswordPal.Services.Database;
 
 public class Context : DbContext
 {
-	private readonly SeedDatabase _seedDatabase;
 	public DbSet<User> User { get; set; }
     public DbSet<StoredPassword> StoredPassword { get; set; }
     public DbSet<PasswordCategory> PasswordCategory { get; set; }
 
-    public Context(DbContextOptions<Context> options) : base(options)
-    {
-	    _seedDatabase = new SeedDatabase();
-    }
-
     public Context()
     {
-	    _seedDatabase = new SeedDatabase();
     }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-	    var configuration = new ConfigurationBuilder()
-			.SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..", "..", "PasswordPal.Services"))
-			.AddJsonFile("appsettings.json", optional: true)
-			.Build();
-	    var connectionString = configuration.GetConnectionString("DefaultConnection");
+		var configuration = new ConfigurationBuilder()
+		.SetBasePath(Path.Combine(Directory.GetCurrentDirectory()))
+		.AddJsonFile("appsettings.json", optional: true)
+		.Build();
+		var connectionString = configuration.GetConnectionString("DefaultConnection");
 
-	    if (!optionsBuilder.IsConfigured)
+		if (!optionsBuilder.IsConfigured)
         {
             optionsBuilder.UseSqlServer(connectionString);
         }
@@ -37,7 +31,138 @@ public class Context : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-	    _seedDatabase.OnModelCreating(modelBuilder);
-        //TODO Include table relationships
-    }
+		modelBuilder.Entity<User>().HasData(
+			new User
+			{
+				Id = 1,
+				Username = "testUser1",
+				Email = "testUser1@email.com",
+				Password = "testPassword1",
+				Salt = "exampleSalt"
+			},
+			new User
+			{
+				Id = 2,
+				Username = "testUser2",
+				Email = "testUser2@email.com",
+				Password = "testPassword2",
+				Salt = "exampleSalt"
+			},
+			new User
+			{
+				Id = 3,
+				Username = "testUser3",
+				Email = "testUser3@email.com",
+				Password = "testPassword3",
+				Salt = "exampleSalt"
+			},
+			new User
+			{
+				Id = 4,
+				Username = "testUser4",
+				Email = "testUser4@email.com",
+				Password = "testPassword4",
+				Salt = "exampleSalt"
+			},
+			new User
+			{
+				Id = 5,
+				Username = "testUser5",
+				Email = "testUser5@email.com",
+				Password = "testPassword5",
+				Salt = "exampleSalt"
+			}
+		);
+
+		modelBuilder.Entity<StoredPassword>().HasData(
+			new StoredPassword
+			{
+				Id = 1,
+				Title = "Title1",
+				Username = "Username1",
+				EncryptedPassword = "encryptedPassword1",
+				Website = "www.website1.com",
+				CreatedAt = "2023-07-04 14:00:00",
+				UpdatedAt = "2023-07-04 14:00:00",
+				UserId = 1
+			},
+			new StoredPassword
+			{
+				Id = 2,
+				Title = "Title2",
+				Username = "Username2",
+				EncryptedPassword = "encryptedPassword2",
+				Website = "www.website2.com",
+				CreatedAt = "2023-07-04 14:00:00",
+				UpdatedAt = "2023-07-04 14:00:00",
+				UserId = 2
+			},
+			new StoredPassword
+			{
+				Id = 3,
+				Title = "Title3",
+				Username = "Username3",
+				EncryptedPassword = "encryptedPassword3",
+				Website = "www.website3.com",
+				CreatedAt = "2023-07-04 14:00:00",
+				UpdatedAt = "2023-07-04 14:00:00",
+				UserId = 3
+			},
+			new StoredPassword
+			{
+				Id = 4,
+				Title = "Title4",
+				Username = "Username4",
+				EncryptedPassword = "encryptedPassword4",
+				Website = "www.website4.com",
+				CreatedAt = "2023-07-04 14:00:00",
+				UpdatedAt = "2023-07-04 14:00:00",
+				UserId = 4
+			},
+			new StoredPassword
+			{
+				Id = 5,
+				Title = "Title5",
+				Username = "Username5",
+				EncryptedPassword = "encryptedPassword5",
+				Website = "www.website5.com",
+				CreatedAt = "2023-07-04 14:00:00",
+				UpdatedAt = "2023-07-04 14:00:00",
+				UserId = 5
+			}
+		);
+
+		modelBuilder.Entity<PasswordCategory>().HasData(
+			new PasswordCategory
+			{
+				Id = 1,
+				Name = "Social",
+			},
+			new PasswordCategory
+			{
+				Id = 2,
+				Name = "Work",
+			},
+			new PasswordCategory
+			{
+				Id = 3,
+				Name = "Streaming",
+			},
+			new PasswordCategory
+			{
+				Id = 4,
+				Name = "Shopping",
+			},
+			new PasswordCategory
+			{
+				Id = 5,
+				Name = "Gaming",
+			},
+			new PasswordCategory
+			{
+				Id = 6,
+				Name = "Financial"
+			}
+		);
+	}
 }
---
+++ PasswordPal.Services/Migrations/20230716105156_InitialSeed.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace PasswordPal.Services.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PasswordCategory",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PasswordCategory", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StoredPassword",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EncryptedPassword = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Website = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UpdatedAt = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StoredPassword", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Salt = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "PasswordCategory",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Social" },
+                    { 2, "Work" },
+                    { 3, "Streaming" },
+                    { 4, "Shopping" },
+                    { 5, "Gaming" },
+                    { 6, "Financial" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "StoredPassword",
+                columns: new[] { "Id", "CategoryId", "CreatedAt", "EncryptedPassword", "Title", "UpdatedAt", "UserId", "Username", "Website" },
+                values: new object[,]
+                {
+                    { 1, 0, "2023-07-04 14:00:00", "encryptedPassword1", "Title1", "2023-07-04 14:00:00", 1, "Username1", "www.website1.com" },
+                    { 2, 0, "2023-07-04 14:00:00", "encryptedPassword2", "Title2", "2023-07-04 14:00:00", 2, "Username2", "www.website2.com" },
+                    { 3, 0, "2023-07-04 14:00:00", "encryptedPassword3", "Title3", "2023-07-04 14:00:00", 3, "Username3", "www.website3.com" },
+                    { 4, 0, "2023-07-04 14:00:00", "encryptedPassword4", "Title4", "2023-07-04 14:00:00", 4, "Username4", "www.website4.com" },
+                    { 5, 0, "2023-07-04 14:00:00", "encryptedPassword5", "Title5", "2023-07-04 14:00:00", 5, "Username5", "www.website5.com" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "User",
+                columns: new[] { "Id", "Email", "Password", "Salt", "Username" },
+                values: new object[,]
+                {
+                    { 1, "testUser1@email.com", "testPassword1", "exampleSalt", "testUser1" },
+                    { 2, "testUser2@email.com", "testPassword2", "exampleSalt", "testUser2" },
+                    { 3, "testUser3@email.com", "testPassword3", "exampleSalt", "testUser3" },
+                    { 4, "testUser4@email.com", "testPassword4", "exampleSalt", "testUser4" },
+                    { 5, "testUser5@email.com", "testPassword5", "exampleSalt", "testUser5" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PasswordCategory");
+
+            migrationBuilder.DropTable(
+                name: "StoredPassword");
+
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
