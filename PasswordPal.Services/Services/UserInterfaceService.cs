8954f1fa50bd2409fe09bddec9364beb6197c841
JRSumner <JoeRaySumner@gmail.com>
2023-08-27 14:31:17 +0100

    Add Logging to all UserInterfaceService Methods.

--- PasswordPal.Services/Services/UserInterfaceService.cs
+++ PasswordPal.Services/Services/UserInterfaceService.cs
@@ -1,22 +1,30 @@
 ﻿using Core.Models;
+using NLog;
 
 namespace PasswordPal.Services.Services;
 
 public class UserInterfaceService
 {
+	private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
 	public static ValidationResult AllFieldsArePopulated(List<string> textBoxString)
 	{
+		Logger.Info("Checking if all fields are populated.");
+
 		var result = new ValidationResult();
 
 		if (!textBoxString.Any(tb => string.IsNullOrEmpty(tb) || string.IsNullOrWhiteSpace(tb)))
 		{
 			result.IsValid = true;
+			Logger.Info("All fields are populated. Validation succeeded.");
 			return result;
 		}
 
 		result.IsValid = false;
 		result.Message = @"Please populate all fields before continuing.";
+		Logger.Warn("Not all fields are populated. Validation failed.");
 
 		return result;
 	}
+
 }
